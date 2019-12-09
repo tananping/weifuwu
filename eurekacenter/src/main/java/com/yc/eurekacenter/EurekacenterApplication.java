23b6b6ed57927494ef9cf107ae1440752726137c
anping <3249899068@qq.com>
2019-12-09 15:30:13 +0800

    微服务模板

---
+++ eurekacenter/src/main/java/com/yc/eurekacenter/EurekacenterApplication.java
@@ -0,0 +1,13 @@
+package com.yc.eurekacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekacenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekacenterApplication.class, args);
+    }
+
+}
